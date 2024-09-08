6dbe8930af75118244a002a9a4660747704c085e
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-09-08 18:47:34 +0200

    Update master.launch.py

--- launch/master.launch.py
+++ launch/master.launch.py
@@ -1,27 +1,31 @@
-from launch import LaunchDescription
-from launch_ros.actions import IncludeLaunchDescription
-from launch_ros.descriptions import PythonLaunchDescriptionSource
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+import os
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    return LaunchDescription([
-        # Include the first launch file
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                get_package_share_directory('repo_controller'),
-                '/launch/controller.launch.py'
-            ]),
-            launch_arguments={}.items()
-        ),
-        
-        # Include the second launch file
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
+    # Include the hand_driver launch file
+    hand_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
                 get_package_share_directory('uclv_seed_robotics_ros'),
-                '/launch/bringup_hand.launch.py'
-            ]),
-            launch_arguments={}.items()
-        ),
+                'launch/bringup_hand.launch.py'
+            )
+        )
+    )
+
+    # Include the controller_driver launch file
+    controller_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('repo_controller'),
+                'launch/controller.launch.py'
+            )
+        )
+    )
+
+    return LaunchDescription([
+        hand_driver_launch,
+        controller_driver_launch
     ])
