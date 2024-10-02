ae96457f00fd48702d3faa9dcd0417f44e70606a
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-10-02 22:15:36 +0200

    Update task_node.cpp

--- src/task_node.cpp
+++ src/task_node.cpp
@@ -15,38 +15,43 @@ public:
             "/cmd/desired_norm_forces", 10);
 
         close_client_ = this->create_client<std_srvs::srv::SetBool>("/close");
-        slipping_client_ = this->create_client<std_srvs::srv::SetBool>("/slipping");
+        slipping_client_ = this->create_client<uclv_seed_robotics_ros_interfaces::srv::SlippingAvoidance>("/slipping");
         open_client_ = this->create_client<std_srvs::srv::SetBool>("/open");
 
-        // // Avvia il task
-        // timer_ = this->create_wall_timer(
-        //     std::chrono::seconds(1), std::bind(&TaskNode::publish_desired_norm_forces, this));
+        this->declare_parameter<std::vector<double>>("desired_norm_data", {0.3, 0.3, 0.3, 0.3, 0.3});
+        this->declare_parameter<std::vector<uint16_t>>("desired_norm_ids", {0, 1, 2, 3, 4});
     }
 
     void run() {
         publish_desired_norm_forces();
         std::cout << "Continue..." << std::endl;
         std::cin.get();
+        
         call_close_service();
         std::cout << "Continue..." << std::endl;
         std::cin.get();
+        
+        call_slipping_service();
+        std::cout << "Continue..." << std::endl;
+        std::cin.get();
+        
         call_open_service();
     }
 
 private:
     rclcpp::Publisher<uclv_seed_robotics_ros_interfaces::msg::Float64WithIdsStamped>::SharedPtr desired_norm_publisher_;
     rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr close_client_;
-    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr slipping_client_;
+    rclcpp::Client<uclv_seed_robotics_ros_interfaces::srv::SlippingAvoidance>::SharedPtr slipping_client_;
     rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr open_client_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    
+    uclv_seed_robotics_ros_interfaces::msg::Float64WithIdsStamped desired_norm_msg_;
 
     void publish_desired_norm_forces()
     {
-        uclv_seed_robotics_ros_interfaces::msg::Float64WithIdsStamped msg;
-        msg.data = {0.3, 0.3, 0.3, 0.3, 0.3};
-        msg.ids = {0, 1, 2, 3, 4};
+        this->get_parameter("desired_norm_data", desired_norm_msg_.data);
+        this->get_parameter("desired_norm_ids", desired_norm_msg_.ids);
 
-        desired_norm_publisher_->publish(msg);
+        desired_norm_publisher_->publish(desired_norm_msg_);
         RCLCPP_INFO(this->get_logger(), "Pubblicato desired_norm_forces");
     }
 
@@ -56,19 +61,18 @@ private:
         request->data = true;
 
         close_client_->async_send_request(request);
+        RCLCPP_INFO(this->get_logger(), "Richiesta servizio close inviata");
     }
 
+    void call_slipping_service()
+    {
+        auto request = std::make_shared<uclv_seed_robotics_ros_interfaces::srv::SlippingAvoidance::Request>();
+        request->data = desired_norm_msg_.data;
+        request->ids = desired_norm_msg_.ids; 
 
-    // void call_slipping_service()
-    // {
-    //     auto request = std::make_shared<uclv_seed_robotics_ros_interfaces::srv::SlippingAvoidance>();
-    //     request->data = {0.3, 0.3, 0.3, 0.3, 0.3};
-    //     request->ids = {0, 1, 2, 3, 4};
-
-    //     slipping_client_->async_send_request(request);
-    // }
-
-
+        slipping_client_->async_send_request(request); 
+        RCLCPP_INFO(this->get_logger(), "Richiesta servizio slipping inviata");
+    }
 
     void call_open_service()
     {
@@ -76,9 +80,8 @@ private:
         request->data = true;
 
         open_client_->async_send_request(request);
+        RCLCPP_INFO(this->get_logger(), "Richiesta servizio open inviata");
     }
-
-
 };
 
 int main(int argc, char **argv)
