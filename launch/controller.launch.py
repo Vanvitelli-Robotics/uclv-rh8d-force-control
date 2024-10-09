1a4c0b523cbdd05bddee03cf069b7d473250dae2
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-10-09 09:21:10 +0200

    Update controller.launch.py

--- launch/controller.launch.py
+++ launch/controller.launch.py
@@ -1,16 +1,32 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    config = os.path.join(
-        get_package_share_directory('repo_controller'),
-        'config',
-        'params.yaml'
+    # Dichiarazione dell'argomento per il file di configurazione
+    config_file_arg = DeclareLaunchArgument(
+        'config_file',
+        default_value='params.yaml',  # Valore predefinito del file di configurazione
+        description='Full path to the parameter file to load'
     )
 
+    # Recupera il percorso del file YAML specificato dall'argomento
+    config_file = LaunchConfiguration('config_file')
+
+    config_dir = os.path.join(
+        get_package_share_directory('repo_controller'),
+        'config'
+    )
+
+    # Unione del percorso della directory con il file specificato
+    config = os.path.join(config_dir, config_file)
+
     return LaunchDescription([
+        config_file_arg,  # Aggiungi l'argomento al LaunchDescription
+
         Node(
             output='screen',
             package='uclv_seed_robotics_ros',
